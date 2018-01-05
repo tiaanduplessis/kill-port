38b2be51eee1308fb72eacfa5ceba129f22e0f3c
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-01-05 14:26:01 +0200

    Add shorthand

--- cli.js
+++ cli.js
@@ -4,6 +4,8 @@
 const kill = require('./')
 const args = require('get-them-args')(process.argv.slice(2))
 
-kill(args.port)
-  .then(() => console.log(`Process on port ${args.port} killed`))
-  .catch(() => console.log(`Could not kill process on port ${args.port}`))
+const port = args.port || args.unknown
+
+kill(port)
+  .then(() => console.log(`Process on port ${port} killed`))
+  .catch(() => console.log(`Could not kill process on port ${port}`))
