ff9e946258862b9b7cbbdccbdf05fe8e21f923c5
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-05-13 21:05:06 +0200

    Format

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict'
 
-const sh = require("execa").shell;
+const sh = require('execa').shell
 
-module.exports = function(port) {
-	if (!Number.parseInt(port)) {
-		return Promise.reject(new Error("Invalid argument provided for port"));
-	}
+module.exports = function (port) {
+  if (!Number.parseInt(port)) {
+    return Promise.reject(new Error('Invalid argument provided for port'))
+  }
 
-	if (process.platform === "win32") {
-		return sh(
-			`Stop-Process -Id (Get-NetTCPConnection -LocalPort ${port}).OwningProcess -Force`
-		);
-	}
+  if (process.platform === 'win32') {
+    return sh(
+      `Stop-Process -Id (Get-NetTCPConnection -LocalPort ${port}).OwningProcess -Force`
+    )
+  }
 
-	return sh(
-		`lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`
-	);
-};
+  return sh(
+    `lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`
+  )
+}
