9c8a523de01ee832caebc9eb044d9aabcd481952
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2018-07-02 05:58:23 +0200

    Add multiple kill ports #10

--- cli.js
+++ cli.js
@@ -4,8 +4,22 @@
 const kill = require('./')
 const args = require('get-them-args')(process.argv.slice(2))
 
-const port = args.port || args.unknown
+const verbose = args.verbose || false
 
-kill(port)
-  .then(() => console.log(`Process on port ${port} killed`))
-  .catch(() => console.log(`Could not kill process on port ${port}`))
+let port = args.port ? args.port.split(',') : args.unknown
+
+if (!Array.isArray(port)) {
+  port = [port]
+}
+
+Promise.all(port.map(current => {
+  return kill(current)
+    .then((result) => {
+      console.log(`Process on port ${current} killed`)
+      verbose && console.log(result)
+    })
+    .catch((error) => {
+      console.log(`Could not kill process on port ${port}`)
+      verbose && console.log(error)
+    })
+}))
