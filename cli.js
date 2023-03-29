6b3d7c1de0e9725fc3e74dd3de5a6a50a22bc716
Roman Prikhodko <r.prih@yandex.ru>
2023-03-29 23:48:05 +0600

    feat: add port ranges

--- cli.js
+++ cli.js
@@ -5,21 +5,57 @@ const kill = require('./')
 const args = require('get-them-args')(process.argv.slice(2))
 
 const verbose = args.verbose || false
-let port = args.port ? args.port.toString().split(',') : args.unknown
 const method = args.method || 'tcp'
 
-if (!Array.isArray(port)) {
-  port = [port]
+const portArgStr = args.port
+	? args.port.toString()
+	: args.unknown.toString()
+
+if (isPortsList(portArgStr)) {
+	const ports = portArgStr.split(",").filter(Boolean)
+	return killAllPorts(ports, { method, verbose })
 }
 
-Promise.all(port.map(current => {
-  return kill(current, method)
-    .then((result) => {
-      console.log(`Process on port ${current} killed`)
-      verbose && console.log(result)
-    })
-    .catch((error) => {
-      console.log(`Could not kill process on port ${port}. ${error.message}.`)
-      verbose && console.log(error)
-    })
-}))
+if (isPortsRange(portArgStr)) {
+	const [min, max] = getMinMaxFromRange(portArgStr)
+	const ports = makeArrayFromRange(min, max)
+	return killAllPorts(ports, { method, verbose })
+}
+
+killAllPorts([portArgStr], { method, verbose })
+
+function killAllPorts(ports, { method, verbose }) {
+	Promise.all(ports.map(current => {
+		return kill(current, method)
+			.then((result) => {
+				console.log(`Process on port ${current} killed`)
+				verbose && console.log(result)
+			})
+			.catch((error) => {
+				console.log(`Could not kill process on port ${current}. ${error.message}.`)
+				verbose && console.log(error)
+			})
+	}))
+}
+
+function isPortsList(argStr) {
+	return argStr.includes(",")
+}
+
+function isPortsRange(argStr) {
+	return argStr.includes("-")
+}
+
+function getMinMaxFromRange(rangeStr) {
+	return rangeStr
+			.split("-")
+			.slice(0, 2)
+			.map(v => parseInt(v))
+			.sort((a, b) => a - b)
+}
+
+function makeArrayFromRange(min, max) {
+	const diff = max - min
+
+	return Array.from({ length: diff + 1 }).map((_, i) => min + i)
+}
